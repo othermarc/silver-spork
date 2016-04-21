eb03e91368bac76e58d21931057b49c61b7a4ba9
othermarc <marcmagerkorth@Marcs-MacBook-Pro.local>
2016-04-21 20:53:53 +1200

    Further updates

--- player.py
+++ player.py
@@ -1,4 +1,17 @@
 class player:
     """docstring for """
-    def __init__(self, name):
+    def __init__(self, name, grade, isMember = False):
         self.name = name
+        self.isMember = isMember
+        self.grade = grade
+
+    def getGrade(self):
+        return self.grade
+
+    def getName(self):
+        return self.name
+
+    def getIsMember(self):
+        return self.isMember
+
+    
